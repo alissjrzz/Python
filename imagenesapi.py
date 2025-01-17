3f5028bd226687738a9b15fce1dee4aceffffddd
alissjrzz <alissondaniella09@gmail.com>
2025-01-17 17:38:06 -0600

    imagenesapi.py

---
+++ imagenesapi.py
@@ -0,0 +1,20 @@
+import requests
+import discord
+from discord.ext import commands
+import os
+import random 
+import bot
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
